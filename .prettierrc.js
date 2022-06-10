81c31d3c81ff984dcb5af80e8442f7d6ea5bee4a
Jiahuis <2889201356@qq.com>
2022-06-10 18:12:23 +0800

    chore: prettier配置修改

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,9 @@ module.exports = {
   trailingComma: 'none', // 行尾逗号,默认none,可选 none|es5|all
   singleQuote: true, // 使用单引号
   arrowParens: 'always', // 箭头函数参数括号 默认avoid 可选 avoid| always
-  jsxBracketSameLine: false, // JSX标签闭合位置 默认false
-  endOfLine: 'auto'
+  jsxBracketSameLine: false, // 在jsx中把'>' 单独放一行
+  bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+  endOfLine: 'auto', // 结尾是 \n \r \n\r auto
+  // printWidth: 100, // 超过最大值换行
+  trailingComma: 'none' // 在对象或数组最后一个元素后面是否加逗号（'es5'在ES5中加尾逗号）
 }
